0230f49ccd3410fc6a57a461a94b29ea6ffff840
imkoushikc <imkoushikc@gmail.com>
2026-03-12 19:20:05 +0800

    Fix base path for GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Change to '/repo-name/' if deploying to GitHub Pages subfolder
+  base: '/koushik-profolio/',
 })
